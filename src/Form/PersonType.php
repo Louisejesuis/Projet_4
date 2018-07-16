609de244c77233ea9cc5b426363d5772ff93232e
Louisejesuis <louise.roy@hotmail.fr>
2018-07-16 10:42:20 +0200

    vich

--- src/Form/PersonType.php
+++ src/Form/PersonType.php
@@ -7,25 +7,38 @@ use App\Entity\Person;
 use App\Entity\Role;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class PersonType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstName')
-            ->add('lastName')
-            ->add('birthDate')
-            ->add('picture')
-            ->add('description')
+            ->add('firstName', TextType::class, array(
+                'label' => "Prénom"
+            ))
+            ->add('lastName', TextType::class, array(
+                'label' => "Nom de famille"))
+            ->add('birthDate',BirthdayType::class, array(
+                'label' => "Date de naissance"))
+            ->add('imageFile', VichImageType::class, array(
+                'label' => "Photo du Wilder"
+            ))
+            ->add('description',TextType::class, array(
+                'label' => "description du wilder"
+            ))
             ->add('role', EntityType::class, array(
                 'class' => Role::class,
-                'choice_label' => 'name'))
+                'choice_label' => 'name',
+                'label' => 'Rôle'))
             ->add('language', EntityType::class, array(
                 'class' => Language::class,
-                'choice_label' => 'name'))        ;
+                'choice_label' => 'name',
+                'label' => 'Langage'))        ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
---
+++ src/Migrations/Version20180716074732.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20180716074732 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE person CHANGE picture image VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE user CHANGE password password VARCHAR(64) NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE person CHANGE image picture VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci');
+        $this->addSql('ALTER TABLE user CHANGE password password VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci');
+    }
+}
